a2e86f41ab57a0719a11fb6d39be6989407c3269
GeorgiyGubarkov <g.v.gubarkov@mpt.ru>
2021-03-29 13:19:22 +0300

    feat: finish task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let newArr = []
+  let discard = false
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if ((element == "--discard-next") && (i != arr.length - 1)) {
+      i+=2
+    } else if ((element == "--discard-prev") && (i != 0)) {
+      newArr.pop()
+    } else if ((element == "--double-next") && (i != arr.length - 1)) {
+       newArr.push(arr[i+1])
+    } else if ((element == "--double-prev") && (i != 0)) {
+      newArr.push(arr[i-1])
+    } else if (["--discard-next", "--discard-prev", "--double-next", "--double-prev"].indexOf(element) != -1) {
+      continue
+    } else {
+      newArr.push(element)
+    }
+  }
+  return newArr
 };
